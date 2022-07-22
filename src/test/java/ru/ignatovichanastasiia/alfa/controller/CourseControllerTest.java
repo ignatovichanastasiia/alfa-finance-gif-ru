b848a4134c36c66590c4fc0e4a068a30f0be42ae
ignatovichanastasiia <ignatovichanastasiia@gmail.com>
2022-07-22 16:52:49 +0300

    more tests for controller

--- src/test/java/ru/ignatovichanastasiia/alfa/controller/CourseControllerTest.java
+++ src/test/java/ru/ignatovichanastasiia/alfa/controller/CourseControllerTest.java
@@ -1,5 +1,6 @@
 package ru.ignatovichanastasiia.alfa.controller;
 
+import java.security.InvalidKeyException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import static org.junit.Assert.*;
@@ -36,31 +37,55 @@ class CourseControllerTest{
 
     @org.junit.jupiter.api.Test
     public void testGetGifRedirect() throws Exception {
-        logger.info("GetGif meth. testing");
+        logger.info("GetGifRedirect meth. testing");
         when(mockCourseService.getGif("SOS")).thenReturn("https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/SOS"))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0"))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
     }
+    
+    @org.junit.jupiter.api.Test
+    public void testGetGifRedirect2() throws Exception {
+        logger.info("GetGifRedirect meth. testing(2)");
+        mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/SO"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
 
     @org.junit.jupiter.api.Test
     public void testGetGifRef() throws Exception {
-        logger.info("GetGifJS meth. testing");
+        logger.info("GetGifRef meth. testing");
         when(mockCourseService.getGifRef("SOS")).thenReturn("https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/ref/SOS"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andExpect(MockMvcResultMatchers.content().string("https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0"));    
     }
+    
+    @org.junit.jupiter.api.Test
+    public void testGetGifRef2() throws Exception {
+        logger.info("GetGifRef meth. testing(2)");
+        
+        mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/ref/SO"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    } 
 
     @org.junit.jupiter.api.Test
     public void testGetGifData() throws Exception {        
-        logger.info("GetJsonGif meth. testing");
+        logger.info("GetGifData meth. testing");
         when(mockCourseService.getGifData("SOS")).thenReturn("Something about gif and url: https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/data/SOS"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andExpect(MockMvcResultMatchers.content().string("Something about gif and url: https://giphy.com/gifs/tiktok-animals-tough-tiktok-WoRz0xf3fUBWTWXUJ0"));
     }
+    
+    @org.junit.jupiter.api.Test
+    public void testGetGifData2() throws Exception {
+        logger.info("GetGifData meth. testing(2)");
+        
+        mockMvc.perform(MockMvcRequestBuilders.get("/course/gif/data/SO"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+    
 }
